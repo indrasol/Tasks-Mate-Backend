c7d4d1b98c8896b3fa4b60544c9f8a30d67f3bf3
Rithin25295 <64233479+Rithin25295@users.noreply.github.com>
2025-09-01 11:20:09 +0000

    chore(db): capture Studio changes from dev [skip ci]

---
+++ supabase/migrations/20250901112008_from-dev-20250901111755.sql
@@ -0,0 +1,21 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_auth_user(p_user_id uuid DEFAULT NULL::uuid, p_username text DEFAULT NULL::text)
+ RETURNS TABLE(id uuid, email text, username text)
+ LANGUAGE sql
+ SECURITY DEFINER
+AS $function$
+    select 
+        u.id,
+        u.email,
+        u.raw_user_meta_data ->> 'username' as username
+    from auth.users u
+    where 
+        (p_user_id is not null and u.id = p_user_id)
+        or
+        (p_username is not null and u.raw_user_meta_data ->> 'username' = p_username)
+    limit 1;
+$function$
+;
+
+
